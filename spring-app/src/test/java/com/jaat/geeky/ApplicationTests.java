6dc76957ce6eb58e6046aa041fa9ff845d68fb5b
madhu <madhu2012dxb@gmail.com>
2022-06-12 10:37:01 +0530

    validating git hooks

--- spring-app/src/test/java/com/jaat/geeky/ApplicationTests.java
+++ spring-app/src/test/java/com/jaat/geeky/ApplicationTests.java
@@ -1,16 +1,39 @@
 package com.jaat.geeky;
 
+import com.jaat.geeky.controllers.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	public void contextLoads() {
+	public void loginSuccess() {
+		MultiValueMap<String, String> formData=new LinkedMultiValueMap<String, String>();
+		formData.add("username","admin");
+		formData.add("password","admin");
+		ResponseEntity<User> responseEntity = restTemplate.postForEntity("/api/login",formData, User.class);
+		assertThat(responseEntity.getBody().getUsername()).isEqualTo("admin");
+	}
+	@Test
+	public void loginFailed() {
+		MultiValueMap<String, String> formData=new LinkedMultiValueMap<String, String>();
+		formData.add("username","admin");
+		formData.add("password","password");
+		ResponseEntity<User> responseEntity = restTemplate.postForEntity("/api/login",formData, User.class);
+		assertThat(responseEntity.getStatusCode().value()).isEqualTo(401);
 	}
 
 }
